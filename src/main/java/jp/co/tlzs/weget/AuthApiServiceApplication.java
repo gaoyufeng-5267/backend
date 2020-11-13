6374d3863e2ab79ccee9c618bb0b096c26bc4d7e
tlzs-zhangxueqian <xueqiang.zhang@tlzs.co.jp>
2020-11-13 15:05:13 +0900

    initail

---
+++ src/main/java/jp/co/tlzs/weget/AuthApiServiceApplication.java
@@ -0,0 +1,11 @@
+package jp.co.tlzs.weget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApiServiceApplication {
+    public static void main(String[] args){
+        SpringApplication.run(AuthApiServiceApplication.class, args);
+    }
+}
